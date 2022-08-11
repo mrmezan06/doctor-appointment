b0d62e2df329ee8b1597dca094619fb2d66a15ae
Mejanur Rahman <mrmezan06@gmail.com>
2022-08-11 06:45:57 +0600

    Bug Fixed for Loading Form Data
    Seperate Both File for Profile and Apply Doctor, now no need to passing data

--- client/src/pages/ApplyDoctor.jsx
+++ client/src/pages/ApplyDoctor.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../redux/alertsSlice";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import DoctorForm from "../components/DoctorForm";
+import { Button, Col, Form, Input, Row, TimePicker } from "antd";
 
 function ApplyDoctor() {
   const dispatch = useDispatch();
@@ -14,15 +14,18 @@ function ApplyDoctor() {
   const onFinish = async (values) => {
     try {
       dispatch(showLoading());
-      const response = await axios.post("/api/user/apply-doctor-account", {
-        ...values,
-        userId: user._id,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
+      const response = await axios.post(
+        "/api/user/apply-doctor-account",
+        {
+          ...values,
+          userId: user._id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         }
-      });
+      );
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
@@ -39,7 +42,114 @@ function ApplyDoctor() {
     <Layout>
       <h1 className="page-title">Apply Doctor</h1>
       <hr />
-      <DoctorForm onFinish={onFinish}/>
+      <Form layout="vertical" onFinish={onFinish}>
+        <div className="card-title">Personal Information</div>
+        <Row gutter={20}>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="First Name"
+              name="firstName"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="First Name" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Last Name"
+              name="lastName"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Last Name" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Phone Number"
+              name="phoneNumber"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Phone Number" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Website"
+              name="website"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Website" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Address"
+              name="address"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Address" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <hr />
+        <div className="card-title">Professional Information</div>
+        <Row gutter={20}>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Specialization"
+              name="specialization"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Specialization" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Experience"
+              name="experience"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Experience" type="number" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Fee Per Consultation"
+              name="feePerConsultation"
+              rules={[{ required: true }]}
+            >
+              <Input
+                type="number"
+                step={50}
+                placeholder="Fee Per Consultation"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Working Hour"
+              name="timings"
+              rules={[{ required: true }]}
+            >
+              <TimePicker.RangePicker />
+            </Form.Item>
+          </Col>
+        </Row>
+        <div className="d-flex justify-content-end">
+          <Button className="primary-button" htmlType="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
     </Layout>
   );
 }
--- client/src/pages/doctor/Profile.jsx
+++ client/src/pages/doctor/Profile.jsx
@@ -1,79 +1,190 @@
-import React, {useEffect, useState} from 'react'
-import DoctorForm from '../../components/DoctorForm'
-import Layout from '../../components/Layout'
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../../redux/alertsSlice";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-hot-toast";
+import { Button, Col, Form, Input, Row } from "antd";
 
 function Profile() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { user } = useSelector((state) => state.user);
-    
-    const [doctor, setDoctor] = useState(null);
-    const params = useParams();
+  const [doctor, setDoctor] = useState(null);
+  const params = useParams();
 
-
-    const onFinish = async (values) => {
-        try {
-            dispatch(showLoading());
-            const response = await axios.post("/api/doctor/update-doctor-profile", {
-              ...values,
-              userId: user._id,
-            },
-            {
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-              }
-            });
-            dispatch(hideLoading());
-            if (response.data.success) {
-              toast.success(response.data.message);
-              navigate("/");
-            } else {
-              toast.error(response.data.message);
-            }
-          } catch (error) {
-            dispatch(hideLoading());
-            toast.error("Something went wrong");
-          }
-    }
-    const getDoctorData = async () => {
-        
-        try {
-            dispatch(showLoading());
-            const res = await axios.post(
-                "/api/doctor/get-doctor-info-by-user-id",
-                { userId: params.userId },
-                { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } }
-              );
-              dispatch(hideLoading());
-              if (res.data.success) {
-                setDoctor(res.data.data);
-                //  return res.data.data;
-              }
-        } catch (error) {
-            dispatch(hideLoading());
+  const onFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.post(
+        "/api/doctor/update-doctor-profile",
+        {
+          ...values,
+          userId: user._id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         }
-      };
-    
-      useEffect( async () => {
-        await getDoctorData();
-        // Eslint-disable-next-line
-      }, []);
-      
+      );
+      dispatch(hideLoading());
+      if (response.data.success) {
+        toast.success(response.data.message);
+        navigate("/");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      toast.error("Something went wrong");
+    }
+  };
+  const getDoctorData = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/doctor/get-doctor-info-by-user-id",
+        { userId: params.userId },
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        setDoctor(res.data.data);
+        //window.location.reload();
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+
+  useEffect(() => {
+    getDoctorData();
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    
     <Layout>
-        <div className='page-title'>Doctor Profile</div>
+      <div className="page-title">Doctor Profile</div>
+      <hr />
+      {/* {doctor && <DoctorForm onFinish={onFinish} initval={doctor}/>} */}
+      {/* {doctor!== null ? console.log(doctor) : console.log('no doctor')} */}
+      {doctor && (
+        <Form layout="vertical" onFinish={onFinish} initialValues={doctor}>
+        <div className="card-title">Personal Information</div>
+        <Row gutter={20}>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="First Name"
+              name="firstName"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="First Name" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Last Name"
+              name="lastName"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Last Name" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Phone Number"
+              name="phoneNumber"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Phone Number" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Website"
+              name="website"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Website" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Address"
+              name="address"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Address" />
+            </Form.Item>
+          </Col>
+        </Row>
         <hr />
-        {/* {doctor && <DoctorForm onFinish={onFinish} initval={doctor}/>} */}
-        {doctor!== null ? console.log(doctor) : console.log('no doctor')}
+        <div className="card-title">Professional Information</div>
+        <Row gutter={20}>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Specialization"
+              name="specialization"
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Specialization" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Experience"
+              name="experience"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Experience" type="number" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Fee Per Consultation"
+              name="feePerConsultation"
+              rules={[{ required: true }]}
+            >
+              <Input
+                type="number"
+                step={50}
+                placeholder="Fee Per Consultation"
+              />
+            </Form.Item>
+          </Col>
+          {/* <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Working Hour"
+              name="timings"
+              rules={[{ required: true }]}
+            >
+              <TimePicker.RangePicker />
+            </Form.Item>
+          </Col> */}
+        </Row>
+        <div className="d-flex justify-content-end">
+          <Button className="primary-button" htmlType="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+      )}
     </Layout>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
